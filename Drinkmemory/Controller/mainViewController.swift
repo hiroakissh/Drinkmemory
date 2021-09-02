365f5245fdbd42cf733397739eaace6ed3e21d90
hirokaissh <hirokai.saito1014@gmail.com>
2021-09-03 00:03:31 +0900

    アカウントのアイコンの設定機能

--- Drinkmemory/Controller/mainViewController.swift
+++ Drinkmemory/Controller/mainViewController.swift
@@ -6,16 +6,99 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
-class mainViewController: UIViewController {
 
+class mainViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var idLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func tapimageView(_ sender: Any) {
+        
+        
+        showAlert()
+        
+    }
     
+    func doCamera(){
+        
+        let sourceType:UIImagePickerController.SourceType = .camera
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.allowsEditing = true
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+            
+        }
+        
+    }
+    
+    func doAlbum(){
+        
+        let sourceType:UIImagePickerController.SourceType = .photoLibrary
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.allowsEditing = true
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if info[.originalImage] as? UIImage != nil {
+            
+            let selectedImage = info[.originalImage] as! UIImage
+            iconImageView.image = selectedImage
+            picker.dismiss(animated: true, completion: nil)
+            
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func showAlert(){
+        
+        let alertController = UIAlertController(title: "選択", message: "どちらを使いますか", preferredStyle: .actionSheet)
+        
+        let actionCamera = UIAlertAction(title: "カメラ", style: .default) { (alert) in
+            self.doCamera()
+        }
+        
+        let actionAlbum = UIAlertAction(title: "アルバム", style: .default) { (alert) in
+            self.doAlbum()
+        }
+        
+        let actionCancel = UIAlertAction(title: "キャンセル", style: .cancel)
+        
+        alertController.addAction(actionCamera)
+        alertController.addAction(actionAlbum)
+        alertController.addAction(actionCancel)
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
 
     /*
     // MARK: - Navigation
