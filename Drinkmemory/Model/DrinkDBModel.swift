67313362772c3b9608edb66c9648a7f2c1c294bd
hirokaissh <hirokai.saito1014@gmail.com>
2021-09-15 21:36:51 +0900

    Firebase jsonDBの設計  

---
+++ Drinkmemory/Model/DrinkDBModel.swift
@@ -0,0 +1,30 @@
+//
+//  DrinkDBModel.swift
+//  Drinkmemory
+//
+//  Created by HiroakiSaito on 2021/09/15.
+//
+
+import Foundation
+import FirebaseStorage
+
+class DrinkDBModel{
+    
+    init(){}
+    
+    //商品の名前、感想、画像を送る
+//    {
+//    "user":{
+//        "coffee":{
+//            "name":"sample",
+//            "image":"imagestoragepath",
+//            }
+//        "blacktea":{
+//            "name":"sample",
+//            "image":"imagestoragepath",
+//            }
+//        }
+//    }
+    
+    
+}
