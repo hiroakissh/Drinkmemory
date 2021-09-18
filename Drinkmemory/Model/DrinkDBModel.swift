99fb7bdaf00bf0b0541e558d8d56e58a8c7ae1ed
hirokaissh <hirokai.saito1014@gmail.com>
2021-09-18 22:44:03 +0900

    DBモデルの作成、読み込み、画像アップロード関数の作成

--- Drinkmemory/Model/DrinkDBModel.swift
+++ Drinkmemory/Model/DrinkDBModel.swift
@@ -6,12 +6,75 @@
 //
 
 import Foundation
+import Firebase
 import FirebaseStorage
 
 class DrinkDBModel{
     
+    let user = Auth.auth().currentUser
+    
+    let db = Firestore.firestore()
+    
     init(){}
     
+    
+    func createDrinkData(_ drinkname: String, _ janlu: String, _ comment: String, _ imageurl: URL){
+        
+        db.collection(user!.uid).document(janlu).setData([
+            "drinkname"    : drinkname,
+            "comment"      : comment,
+            "drinkimageurl": imageurl
+        ])
+        
+    }
+    
+    func readDrinkData(_ janlu: String){
+        
+        db.collection(user!.uid).document(janlu).getDocument { (DocumentSnapshot, error) in
+            
+            if error != nil{
+                print(error.debugDescription)
+                return
+            }else{
+                
+               // if let value =
+                
+            }
+            
+        }
+        
+    }
+    
+    func uploaddrinkimage(data:Data,_ drinkname:String){
+        
+        let image = UIImage(data: data)
+        let drinkImage = image?.jpegData(compressionQuality: 0.1)
+        
+        let storage = Storage.storage()
+        let storageRef = storage.reference().child("drinkimage/" + user!.uid + drinkname + ".jpg")
+        
+        storageRef.putData(drinkImage!, metadata: nil) { (metaData, error) in
+            
+            if error != nil{
+                print(error.debugDescription)
+                return
+            }
+            
+            storageRef.downloadURL { (url, error) in
+                
+                if error != nil{
+                    print(error.debugDescription)
+                    return
+                }
+                
+                UserDefaults.standard.setValue(url?.absoluteString, forKey: "drinkimage")
+                
+            }
+            
+        }
+        
+        
+    }
     //商品の名前、感想、画像を送る
 //    {
 //    "user":{
